fa480c95587be640e5a25183d6472dacbd4a98ec
jakubiszon26 <itcppprograming@gmail.com>
2021-02-19 18:23:31 +0100

    no styled signup form base

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/Home.vue'
 import About from '../views/about/About.vue';
+import Signup from '../views/signup/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: Signup
   }
 ]
 
---
+++ frontend/src/views/signup/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit="checkForm">
+    <label for="newLogin">New login</label>
+    <input
+      name="newLogin"
+      placeholder="for example: doglover12"
+      v-model="newLogin"
+      type="text"
+    />
+    <br />
+    <label for="newPassword">New Password</label>
+    <input
+      name="newPassword"
+      placeholder="Don't use the same password on multiple websites"
+      v-model="newPassword"
+      type="password"
+    />
+    <br />
+    <button type="submit">Sign up</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      newLogin: "",
+      newPassword: ""
+    };
+  },
+  methods: {
+    handleFormSubmit() {
+      //just console.log for now to see is it working
+      console.log(this.newLogin);
+      console.log(this.newPassword);
+    }
+  }
+};
+</script>
+
+<style></style>
